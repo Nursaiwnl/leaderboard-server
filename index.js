66dfd16133b1b1599e05074670c9b06c4b778527
Nursaiwnl <104405362+Nursaiwnl@users.noreply.github.com>
2025-05-19 18:54:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const FILE = './leaderboard.json';
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/leaderboard', (req, res) => {
+    if (fs.existsSync(FILE)) {
+        const data = JSON.parse(fs.readFileSync(FILE, 'utf-8'));
+        res.json(data);
+    } else {
+        res.json([]);
+    }
+});
+
+app.post('/save-score', (req, res) => {
+    const { name, score } = req.body;
+    if (!name || typeof score !== 'number') return res.status(400).send('Invalid data');
+
+    let data = [];
+    if (fs.existsSync(FILE)) {
+        data = JSON.parse(fs.readFileSync(FILE, 'utf-8'));
+    }
+
+    data.push({ name, score });
+    data.sort((a, b) => b.score - a.score);
+    data = data.slice(0, 10);
+
+    fs.writeFileSync(FILE, JSON.stringify(data, null, 2));
+    res.sendStatus(200);
+});
+
+app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
